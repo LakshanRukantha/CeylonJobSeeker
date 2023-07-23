635b6a5a9ca76d97303bfbf53219c63e6c5c2c13
Janu Ayodya <118840840+januayodya@users.noreply.github.com>
2023-07-23 15:30:11 +0530

    admin panel 3rd edit

---
+++ styles/admin-panel.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.google-btn').addEventListener('click', function() {
+    // Redirect the user to the Google sign-in page
+    window.location.href = 'https://accounts.google.com/';
+});
