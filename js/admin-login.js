23cd1adb6fb807917a01693b09094935fec35048
Janu Ayodya <118840840+januayodya@users.noreply.github.com>
2023-07-25 03:17:12 +0530

    btn edit

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,25 +1,25 @@
-
-document.querySelector('.google-btn').addEventListener('click', function() {
+var googleBtn = document.querySelector('.google-btn');
+googleBtn.addEventListener('click', function() {
     // Redirect the user to the Google sign-in page
     window.location.href = 'https://accounts.google.com/';
 });
-const inputs = document.querySelectorAll(".input");
 
+var inputs = document.querySelectorAll(".input");
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
+function addcl() {
+    var parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
 }
 
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
+function remcl() {
+    var parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
 }
 
-//user name password style
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+// Adding event listeners to input fields
+inputs.forEach(function(input) {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
 });
