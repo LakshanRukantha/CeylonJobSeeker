ecd7ae32c9c311b5aba2370845bfa559544b8022
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-07-30 21:05:04 +0530

    feat: Create New Component For Navbar

---
+++ components/navbar.php
@@ -0,0 +1,60 @@
+
+      <!-- Navbar Start -->
+      <nav class="navbar navbar-expand-lg fixed-top w-full bg-body-tertiary">
+        <div class="container-fluid px-lg-4">
+          <a class="navbar-brand" href="index.html"
+            ><img
+              class="img-logo"
+              src="./assets/images/logo.svg"
+              alt="CJS Logo"
+          /></a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href="./index.html"
+                  >Home</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./categories.php">Categories</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./faq.php">FAQ</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./contact.php">Contact Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./about.php">About Us</a>
+              </li>
+            </ul>
+            <div>
+            <a href="admin-register.php"
+              ><button class="btn btn-primary" type="button">
+                Register
+              </button></a
+            >
+            <a class="mx-1" href="admin-login.php"
+              ><button class="btn btn-outline-primary" type="button">
+                Login
+              </button></a
+            >
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!-- Navbar End -->
--- faq.php
+++ faq.php
@@ -16,58 +16,7 @@
   </head>
   <body>
     <div class="main-wrapper">
-      <!-- Navbar Start -->
-      <nav class="navbar navbar-expand-lg fixed-top w-full bg-body-tertiary">
-        <div class="container-fluid px-lg-4">
-          <a class="navbar-brand" href="index.html"
-            ><img
-              class="img-logo"
-              src="./assets/images/logo.svg"
-              alt="CJS Logo"
-          /></a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a
-                  class="nav-link active"
-                  aria-current="page"
-                  href="./index.html"
-                  >Home</a
-                >
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="categories.html">Categories</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="faq.html">FAQ</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.html">About Us</a>
-              </li>
-            </ul>
-            <a href="./admin-login.php"
-              ><button class="btn-primary" type="button">
-                Login As Admin
-              </button></a
-            >
-          </div>
-        </div>
-      </nav>
-      <!-- Navbar End -->
+    <?php include_once "./components/navbar.php"; ?>
       <div class="container faq-section">
         <div class="faq-section-left">
           <img
