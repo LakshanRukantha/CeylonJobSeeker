af8c576a184a6218f663b9b2c60612cc38c20a50
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-08-19 17:11:09 +0530

    Add Favicon

--- contact.php
+++ contact.php
@@ -2,6 +2,26 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <meta viewport="width=device-width, initial-scale=1.0" />
+    <!-- Favicon -->
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="./assets/images/favicons/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="./assets/images/favicons/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="./assets/images/favicons/favicon-16x16.png"
+    />
+    <link rel="manifest" href="./assets/images/favicons/site.webmanifest" />
     <!--                     Bootstrap CSS - CDN               -->
 
     <link
@@ -94,7 +114,7 @@
 
               <div>
                 <br />
-                <button type="button" class="btn-primary d-inline-flex align-items-center gap-2"><i class="fa-regular fa-paper-plane"></i>Send Message</button>
+                <button type="submit" class="btn-primary d-inline-flex align-items-center gap-2"><i class="fa-regular fa-paper-plane"></i>Send Message</button>
               </div>
             </form>
 
